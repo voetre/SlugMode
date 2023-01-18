97bcf04c454660d1b3414e7fd4972c41cef8fc40
voetre <39429413+voetre@users.noreply.github.com>
2023-01-19 03:09:58 +1300

    start

--- SlugMode/Plugin.cs
+++ SlugMode/Plugin.cs
@@ -1,22 +1,26 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
-using System.Reflection;
 using Dalamud.Interface.Windowing;
-using SamplePlugin.Windows;
+using SlugMode.Windows;
+using System;
+using Dalamud.Game.ClientState.Objects.SubKinds;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
 
-namespace SamplePlugin
+namespace SlugMode
 {
-    public sealed class Plugin : IDalamudPlugin
+    public unsafe class Plugin : IDalamudPlugin
     {
-        public string Name => "Sample Plugin";
-        private const string CommandName = "/pmycommand";
+        public string Name => "Slug Mode";
+        private const string CommandName = "/slugmode";
 
         private DalamudPluginInterface PluginInterface { get; init; }
         private CommandManager CommandManager { get; init; }
         public Configuration Configuration { get; init; }
-        public WindowSystem WindowSystem = new("SamplePlugin");
+        public WindowSystem WindowSystem = new("SlugMode");
+        public static IntPtr PlayerAddress => DalamudApi.ClientState.LocalPlayer.Address;
 
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
@@ -32,8 +36,12 @@ namespace SamplePlugin
             var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
             var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
 
+
+            var imagePath2 = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+            var goatImage2 = this.PluginInterface.UiBuilder.LoadImage(imagePath);
+
             WindowSystem.AddWindow(new ConfigWindow(this));
-            WindowSystem.AddWindow(new MainWindow(this, goatImage));
+            WindowSystem.AddWindow(new MainWindow(this, goatImage, goatImage2));
 
             this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
             {
@@ -42,6 +50,22 @@ namespace SamplePlugin
 
             this.PluginInterface.UiBuilder.Draw += DrawUI;
             this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+        }
+        public static void SlugMode(int ModelID)
+        {
+            byte[] Bytes_ModelID = BitConverter.GetBytes(ModelID);
+            var SlugsMadeCount = 0;
+            foreach (var o in DalamudApi.ObjectTable)
+            {
+                if (o is not PlayerCharacter || o == DalamudApi.ClientState.LocalPlayer) continue;
+                var obj = (GameObject*)o.Address;
+                var p = (Character*)o.Address;
+                obj->RenderFlags = 2;
+                p->ModelCharaId = ModelID;
+                DalamudApi.Framework.RunOnTick(() => obj->RenderFlags = 0, default, 2);
+                SlugsMadeCount++;
+            }
+            DalamudApi.ChatGui.PrintError($"{SlugsMadeCount}");
         }
 
         public void Dispose()
@@ -53,7 +77,8 @@ namespace SamplePlugin
         private void OnCommand(string command, string args)
         {
             // in response to the slash command, just display our main ui
-            WindowSystem.GetWindow("My Amazing Window").IsOpen = true;
+            //WindowSystem.GetWindow("My Amazing Window").IsOpen = true;
+            SlugMode(Int32.Parse(args));
         }
 
         private void DrawUI()
--- SlugMode/Windows/MainWindow.cs
+++ SlugMode/Windows/MainWindow.cs
@@ -1,26 +1,23 @@
-﻿using System;
+using System;
 using System.Numerics;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 using ImGuiScene;
 
-namespace SamplePlugin.Windows;
+namespace SlugMode.Windows;
 
 public class MainWindow : Window, IDisposable
 {
     private TextureWrap GoatImage;
+    private TextureWrap GoatImage2;
     private Plugin Plugin;
 
-    public MainWindow(Plugin plugin, TextureWrap goatImage) : base(
+    public MainWindow(Plugin plugin, TextureWrap goatImage, TextureWrap goatImage2) : base(
         "My Amazing Window", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
-        this.SizeConstraints = new WindowSizeConstraints
-        {
-            MinimumSize = new Vector2(375, 330),
-            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
-        };
 
         this.GoatImage = goatImage;
+        this.GoatImage2 = goatImage2;
         this.Plugin = plugin;
     }
 
@@ -38,11 +35,13 @@ public class MainWindow : Window, IDisposable
             this.Plugin.DrawConfigUI();
         }
 
-        ImGui.Spacing();
-
-        ImGui.Text("Have a goat:");
-        ImGui.Indent(55);
         ImGui.Image(this.GoatImage.ImGuiHandle, new Vector2(this.GoatImage.Width, this.GoatImage.Height));
-        ImGui.Unindent(55);
+
+        ImGui.SameLine();
+        ImGui.Image(this.GoatImage2.ImGuiHandle, new Vector2(this.GoatImage2.Width, this.GoatImage2.Height));
+
+        if (ImGui.Button("Slug Mode")){
+            Plugin.SlugMode(50);
+        }
     }
 }
